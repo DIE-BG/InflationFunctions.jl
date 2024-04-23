e1c45dafc338035789aa09ae3948c016ff47d986
Rodrigo Chang <rrcp777@gmail.com>
2024-04-23 10:21:54 -0600

    Implemented `r` parameter in  Gaussian smoothing functions

--- src/InflationGaussianSmoothingEq.jl
+++ src/InflationGaussianSmoothingEq.jl
@@ -2,38 +2,42 @@
 
 """
     InflationGSEq <: InflationFunction
-    InflationGSEq(k, s1, s2)
+    InflationGSEq(k, s1, s2, r=2)
 
 Computes a Gaussian smoothing with equal weights according to quantile `k`, left-standard
 deviation `s1`, and right-standard deviation `s2`. The item at the position of the `k`
 quantile receives a higher weight and the items to the left or right are downweighted using
-a Gaussian bell function with different standard deviations to the left and right.
+a Gaussian bell function with different standard deviations to the left and right. The bell
+function is shaped by the power `r`.
 
-There are ``n`` items in the CPI database of monthly price changes. If ``p`` is the index of
-the desired quantile, the standard deviation is defined by `s(x,p) = x <= p ? s1*n : s2*n`
-and the smoothing function by `f(x,p) = exp(-(x-p)^2 / s(x,p)^2)`.
+If ``k`` is the desired centering quantile, the standard deviation is a simple piecewise
+function defined by `s(x,k) = x <= p ? 100s1 : 100s2` and the smoothing function by `f(x,p,r)
+= exp(-abs((x-p) / s(x,p))^r)`.
 """
 Base.@kwdef struct InflationGSEq <: InflationFunction
     k::Float32
     s1::Float32
     s2::Float32
+    r::Int
 end
 
-InflationGSEq(k::Real, s1::Real,s2::Real) = InflationGSEq(
+InflationGSEq(k::Real, s1::Real,s2::Real, r::Int=2) = InflationGSEq(
     k = Float32(k), 
     s1 = Float32(s1), 
-    s2 = Float32(s2)
+    s2 = Float32(s2),
+    r = r,
 )
 
-function InflationGSEq(params::Vector{<:Real})
+function InflationGSEq(params::Vector{<:Real}, r::Int=2)
     length(params) != 3 && error("Expected 3 parameters")
-    InflationGSEq(convert.(Float32, params))
+    InflationGSEq(convert.(Float32, params), r)
 end
 
 function (inflfn::InflationGSEq)(base::VarCPIBase{T}) where T     
     s1 = inflfn.s1
     s2 = inflfn.s2
-    k = inflfn.k 
+    k = 100 * inflfn.k 
+    r = inflfn.r
 
     # summary monthly inflation 
     vm = Vector{T}(undef, periods(base)) 
@@ -41,12 +45,11 @@ function (inflfn::InflationGSEq)(base::VarCPIBase{T}) where T
     # p is the index of the desired percentile
     n = items(base)
     cew = (100 / n) * (1:n)
-    p = findfirst(>=(100k), cew)
 
     # Standard deviation
-    s(x,p) = x <= p ? 100s1 : 100s2 
-    # Gaussian smoothing function around p
-    f(x, p) = exp(-abs((x - p) / s(x, p))^3)
+    s(x,k) = x <= k ? 100s1 : 100s2 
+    # Gaussian smoothing function around k with power r
+    f(x,k,r) = exp(-abs((x-k) / s(x,k))^r) 
 
     # For every t, we sort and smooth the weights to compute the summary
     Threads.@threads for i in 1:periods(base)
@@ -56,8 +59,8 @@ function (inflfn::InflationGSEq)(base::VarCPIBase{T}) where T
         vo = v[o]
 
         # Apply smoothing while computing the summary 
-        cnorm = sum(f(j, p) for j in 1:n)
-        @inbounds vm[i] = sum(vo[j] * f(j, p) for j in 1:n) / cnorm 
+        cnorm = sum(f(cew[j], k, r) for j in 1:n)
+        @inbounds vm[i] = sum(vo[j] * f(cew[j], k, r) for j in 1:n) / cnorm 
     end
 
     vm
@@ -67,7 +70,8 @@ function measure_name(inflfn::InflationGSEq)
     p = string(round(inflfn.k, digits=2))
     s1 = string(round(inflfn.s1, digits=2))
     s2 = string(round(inflfn.s2, digits=2))
-    "Suavizamiento Gausiano Equiponderado ($p, $s1, $s2)"
+    r = inflfn.r
+    "Suavizamiento Gausiano Equiponderado ($p, $s1, $s2, $r)"
 end
 
-CPIDataBase.params(inflfn::InflationGSEq) = (inflfn.k, inflfn.s1, inflfn.s2)
+CPIDataBase.params(inflfn::InflationGSEq) = (inflfn.k, inflfn.s1, inflfn.s2, inflfn.r)
